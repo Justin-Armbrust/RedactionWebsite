38bfc25932ceff00d6a818e2d71ef50ec4bfebc5
Justin-Armbrust <superjca2000@gmail.com>
2020-01-29 18:29:33 -0800

    update

--- redaction_website/redaction_website/settings.py
+++ redaction_website/redaction_website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS =[ os.path.join(BASE_DIR,'redaction_website/templates/WebsiteFiles'),]
+
+django_heroku.settings(locals())
